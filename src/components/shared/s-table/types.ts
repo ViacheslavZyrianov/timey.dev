615b9448ce0789b89d8a4f57fa7f39327f795569
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-04-26 23:13:32 +0300

    add types for table

---
+++ src/components/shared/s-table/types.ts
@@ -0,0 +1,10 @@
+export type TypeTableHeader = {
+  key: string
+  label: string
+}
+
+export type TypeTableRow = {
+  id: string;
+} & {
+  [key: string]: string | number | boolean | string[] | null;
+};
