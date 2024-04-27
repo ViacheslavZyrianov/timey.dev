7842c12d8fdcb09e37c153478c9bff101392455c
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-04-27 16:01:19 +0300

    add path: ':team_id/members/:member_id',

--- src/router/routes/teams.ts
+++ src/router/routes/teams.ts
@@ -9,7 +9,12 @@ export default {
     {
       path: ':team_id',
       name: 'team',
-      component: () => import('@/views/Teams/Team/index.vue')
+      component: () => import('@/views/Teams/Team/index.vue'),
+    },
+    {
+      path: ':team_id/members/:member_id',
+      name: 'team-member',
+      component: () => import('@/views/Teams/Team/Members/index.vue'),
     },
     {
       path: 'new',
