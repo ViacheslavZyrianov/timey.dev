311f43b3e51620b22d39f4b2abcf40b5563da407
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-04-27 16:03:18 +0300

    update table

--- src/views/Teams/Team/index.vue
+++ src/views/Teams/Team/index.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { useRoute } from "vue-router"
+import { useRoute, useRouter } from "vue-router"
 import useTeamsStore from "@/store/teams"
 import tableHeaders from "./tableHeaders";
 import { computed, ComputedRef, ref, Ref } from "vue";
 
 const route = useRoute()
+const router = useRouter()
 const teamsStore = useTeamsStore()
 
 const uid: Ref<string> = ref('')
@@ -19,25 +20,47 @@ const team = await teamsStore.fetchTeam(teamId.value)
 
 const teamMembersRequests = team.members ? team.members.map((member) => teamsStore.fetchTeamMemberById(member)) : null
 const teamMembers = teamMembersRequests ? await Promise.all(teamMembersRequests) : []
+
+const onRedirectToTeamMemberDetails = (member_id: string) => {
+  router.push({
+    name: 'team-member',
+    params: {
+      team_id: team.id,
+      member_id
+    }
+  })
+}
 </script>
 
 <template>
-  <div class="d-flex direction-column">
+  <div class="d-flex direction-column full-width">
     <h1>{{ team.name }}</h1>
     <p>{{ team.description }}</p>
-    <form @submit.prevent="onSubmitAddTeamMember">
+    <form class="d-flex ml-auto mb-8" @submit.prevent="onSubmitAddTeamMember">
       <s-input
         v-model="uid"
+        placeholder="Enter user id"
+        width="300px"
       />
       <s-button
         type="submit"
         icon="mdiAccountPlusOutline"
         title="Add team member"
+        class="ml-8"
       />
     </form>
     <s-table
       :headers="tableHeaders"
       :rows="teamMembers"
-    />
+    >
+      <template #actions="{ row }">
+        <s-button
+          size="small"
+          title="View Member"
+          icon="mdiEyeOutline"
+          @click="onRedirectToTeamMemberDetails(row.id)"
+        />
+      </template>
+    </s-table>
   </div>
 </template>
