9421714d49f53227c5de2ff9dbbf8fce5cd621d7
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-04-27 16:01:35 +0300

    update table headers

--- src/views/Teams/Team/tableHeaders.ts
+++ src/views/Teams/Team/tableHeaders.ts
@@ -2,9 +2,14 @@ export default [
   {
     key: 'id',
     label: 'ID',
+    width: '0'
   },
   {
     key: 'displayName',
-    label: 'Name',
+    label: 'Name'
+  },
+  {
+    key: 'actions',
+    width: '0'
   }
 ]
