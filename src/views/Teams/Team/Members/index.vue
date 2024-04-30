824e4a753096c4eda6bd45a73f2002fac7c9a5bf
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-04-30 19:12:20 +0300

    add basic teams member page

---
+++ src/views/Teams/Team/Members/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template>
+<pre>{{route.params}}</pre>
+</template>
+
+<style scoped lang="scss">
+
+</style>
