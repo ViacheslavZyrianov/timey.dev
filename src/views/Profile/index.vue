c7d25067da839322e95a6112a5191aa28e13830a
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-05-05 15:45:03 +0300

    redesign page profile

--- src/views/Profile/index.vue
+++ src/views/Profile/index.vue
@@ -5,13 +5,10 @@ const authStore = useAuthStore()
 </script>
 
 <template>
-  <div class="d-flex flex-grow-1 flex-column full-width">
-    <div class="d-flex align-center mb-8">
-      <s-avatar rounded :src="authStore.user.photoURL" class="mr-8" />
-      <h1>{{ authStore.user.displayName }}</h1>
-    </div>
+  <div class="d-flex align-center mb-8">
+    <s-avatar rounded :src="authStore.user.photoURL" class="mr-8" />
+    <h1>{{ authStore.user.displayName }}</h1>
   </div>
-
 </template>
 
 <style scoped lang="scss">
