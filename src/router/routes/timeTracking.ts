30de8f1cb675247dfeecb99c1eb3b1593a21d916
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-05-05 19:03:10 +0300

    fix route '/time-tracking'

--- src/router/routes/timeTracking.ts
+++ src/router/routes/timeTracking.ts
@@ -1,5 +1,20 @@
 export default {
   path: '/time-tracking',
   name: 'timeTracking',
-  component: () => import('@/views/TimeTracking/index.vue')
+  redirect: () => {
+    return {
+      name: 'timeTrackingYearMonth',
+      params: {
+        year: (new Date()).getFullYear(),
+        month: (new Date()).getMonth() + 1,
+      }
+    }
+  },
+  children: [
+    {
+      path: ':year/:month',
+      name: 'timeTrackingYearMonth',
+      component: () => import('@/views/TimeTracking/index.vue')
+    }
+  ]
 }
