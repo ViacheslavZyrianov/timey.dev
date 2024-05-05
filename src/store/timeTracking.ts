4374df69633c3e6b4db6b8df97490771b0d24911
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-05-05 19:02:53 +0300

    fix fetchTimeTracking

--- src/store/timeTracking.ts
+++ src/store/timeTracking.ts
@@ -1,13 +1,13 @@
 import { defineStore } from "pinia";
 import useAuthStore from '@/store/auth'
-import { fetchSubCollectionItems } from "@/utils/firebaseRequestor";
+import { fetchTimeTrackingByMonth } from "@/utils/firebaseRequestor";
 import { TypeTimeTrackingItemRead } from "@/types/time-tracking";
 
 const authStore = useAuthStore();
 
 export default defineStore('timeTracking', () => {
-  async function fetchTimeTracking():Promise<TypeTimeTrackingItemRead[]> {
-    return await fetchSubCollectionItems<TypeTimeTrackingItemRead>('users', authStore.user.uid, 'time-tracking')
+  async function fetchTimeTracking(month: number, year: number):Promise<TypeTimeTrackingItemRead[]> {
+    return await fetchTimeTrackingByMonth<TypeTimeTrackingItemRead>(month, year)
   }
 
   return { fetchTimeTracking }
