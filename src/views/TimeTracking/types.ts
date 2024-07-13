68ab7420927b4ec05607f824f8807f9579fd0491
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-07-13 20:02:31 +0300

    remove type TypeDayData

--- src/views/TimeTracking/types.ts
+++ src/views/TimeTracking/types.ts
@@ -1,11 +1,5 @@
-import {TypeTimeTrackingItemRead} from "@/types/time-tracking";
-
 export type TypeTaskInDayData = {
-  hours: Pick<TypeTimeTrackingItemRead, 'hours'>
-  task: Pick<TypeTimeTrackingItemRead, 'task'>
-}
-
-export type TypeDayData = {
-  id: Pick<TypeTimeTrackingItemRead, 'id'>,
-  tasks: TypeTaskInDayData[]
+  id: string,
+  hours: number
+  task: string
 }
