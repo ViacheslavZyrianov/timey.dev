8ef66f6006861897834317803d717c671312af01
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-07-14 18:30:44 +0300

    improve generating months

--- src/views/TimeTracking/dates.ts
+++ src/views/TimeTracking/dates.ts
@@ -1,17 +1,16 @@
-export const months = [
-  { label: 'January', value: '1' },
-  { label: 'February', value: '2' },
-  { label: 'March', value: '3' },
-  { label: 'April', value: '4' },
-  { label: 'May', value: '5' },
-  { label: 'June', value: '6' },
-  { label: 'July', value: '7' },
-  { label: 'August', value: '8' },
-  { label: 'September', value: '9' },
-  { label: 'October', value: '10' },
-  { label: 'November', value: '11' },
-  { label: 'December', value: '12' }
-]
+import dayjs from "dayjs";
+import localeData from 'dayjs/plugin/localeData'
+import {TypeSelectItem} from "@/components/shared/select";
+
+dayjs.extend(localeData)
+
+export const months: TypeSelectItem[] = dayjs()
+  .localeData()
+  .months()
+  .map((month: string, index: number) => ({
+    label: month,
+    value: `${index + 1}`,
+  }))
 
 export const years = [
   {
