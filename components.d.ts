90d87eaf91805e295217524e1048ec9e00b2dfdb
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-07-17 00:52:55 +0300

    create s-switch.vue

--- components.d.ts
+++ components.d.ts
@@ -23,6 +23,7 @@ declare module 'vue' {
     SIcon: typeof import('./src/components/shared/s-icon.vue')['default']
     SInput: typeof import('./src/components/shared/s-input.vue')['default']
     SSelect: typeof import('./src/components/shared/s-select.vue')['default']
+    SSwitch: typeof import('./src/components/shared/s-switch.vue')['default']
     STable: typeof import('./src/components/shared/s-table/index.vue')['default']
     SToast: typeof import('./src/components/shared/s-toast.vue')['default']
     Weekdays: typeof import('./src/components/shared/s-calendar/weekdays.vue')['default']
---
+++ src/components/shared/s-switch.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+  import { computed } from "vue";
+
+  const model = defineModel({
+    type: Boolean,
+    default: false,
+  });
+
+  const sSwitchClassList = computed(() => [
+    "s-switch d-flex align-center flex-column-gap-4",
+    `s-switch--${model.value}`,
+  ]);
+
+  const onToggleSwitch = () => {
+    model.value = !model.value;
+  };
+</script>
+
+<template>
+  <div
+    :class="sSwitchClassList"
+    @click="onToggleSwitch"
+  >
+    <slot name="prepended-text" />
+    <div class="switcher">
+      <div class="switcher-dot" />
+    </div>
+    <slot name="appended-text" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  $size: 14px;
+  $paddings: 4px;
+
+  .s-switch {
+    .switcher {
+      width: calc(($size * 2) + ($paddings * 2));
+      height: calc($size + ($paddings * 2));
+      cursor: pointer;
+      border-radius: calc($size - ($paddings / 2));
+      padding: $paddings;
+      transition: background-color 0.3s;
+
+      &-dot {
+        width: $size;
+        height: $size;
+        background-color: $c-primary;
+        border-radius: 50%;
+        transition: transform 0.3s;
+      }
+    }
+
+    &--true {
+      .switcher {
+        background-color: lighten($c-success, 40%);
+
+        &-dot {
+          background-color: $c-success;
+          transform: translateX(100%);
+        }
+      }
+    }
+
+    &--false {
+      .switcher {
+        background-color: lighten($c-error, 20%);
+
+        &-dot {
+          background-color: $c-error;
+        }
+      }
+    }
+  }
+</style>
