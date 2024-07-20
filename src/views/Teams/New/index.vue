ff6e151fd366456ef4a8cdd09b5a9446cb969dc8
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-07-20 17:26:10 +0300

    refactor create team page

--- src/views/Teams/New/index.vue
+++ src/views/Teams/New/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  import { reactive, ref, Ref, watch } from "vue";
+  import { reactive, ref, Ref } from "vue";
   import { useRouter } from "vue-router";
   import useTeamsStore from "@/store/teams";
   import { TypeTeamCreate } from "@/types/teams";
@@ -7,19 +7,13 @@
   const router = useRouter();
   const teamsStore = useTeamsStore();
 
-  const isSubmitButtonDisabled: Ref<boolean> = ref(true);
+  const isButtonSubmitDisabled: Ref<boolean> = ref(false);
+  const isButtonSubmitLoading: Ref<boolean> = ref(false);
 
   const form: TypeTeamCreate = reactive({
     name: "",
     description: "",
   });
-
-  watch(
-    () => form.name,
-    () => {
-      isSubmitButtonDisabled.value = form.name.length < 2;
-    },
-  );
 
   const onAddNewTeam = async () => {
     const id = await teamsStore.postTeam(form);
@@ -28,33 +22,29 @@
 </script>
 
 <template>
-  <s-card
-    class="d-flex direction-column mb-auto flex-gap-12"
-    width="400px"
-  >
-    <template #title> Add Team </template>
-    <template #content>
-      <s-input
-        v-model="form.name"
-        label="Name"
-        placeholder="Enter name"
-        class="mb-8"
-      />
-      <s-input
-        v-model="form.description"
-        label="Description"
-        placeholder="Enter description"
-        class="mb-8"
-      />
-      <s-button
-        size="large"
-        :disabled="isSubmitButtonDisabled"
-        @click="onAddNewTeam"
-      >
-        Create new team
-      </s-button>
-    </template>
-  </s-card>
+  <h1 class="page-title">Create team</h1>
+
+  <div class="width-50-p">
+    <s-input
+      v-model="form.name"
+      label="Name"
+      placeholder="Enter name"
+      class="mb-8"
+    />
+    <s-input
+      v-model="form.description"
+      label="Description"
+      placeholder="Enter description"
+      class="mb-8"
+    />
+    <s-button
+      :disabled="isButtonSubmitDisabled"
+      :loading="isButtonSubmitLoading"
+      @click="onAddNewTeam"
+    >
+      Create new team
+    </s-button>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
