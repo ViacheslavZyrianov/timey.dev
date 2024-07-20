52c3bfdd8382f32db8646cef42239a52986f45a1
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-07-20 22:04:09 +0300

    add s-empty.vue component

---
+++ src/components/shared/s-empty.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <s-card class="s-empty">
+    <template #content>
+      <div class="d-flex flex-column align-center width-100">
+        <img
+          src="@/assets/img/empty-data.svg"
+          alt="Empty data"
+          class="mb-16"
+        />
+        <div class="page-title opacity-70">No data available</div>
+      </div>
+    </template>
+  </s-card>
+</template>
+
+<style scoped lang="scss">
+  .s-empty {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 400px;
+
+    img {
+      //width: 30%;
+      max-width: 300px;
+    }
+  }
+</style>
