22cc94d2719d3edd6d30d367b7ecc9ef8e07b685
ViacheslavZyrianov <viacheslavz.dev@support.team>
2024-08-17 23:54:31 +0300

    update paths

--- src/router/routes/teams.ts
+++ src/router/routes/teams.ts
@@ -10,12 +10,12 @@ export default {
       component: () => import("@/views/Teams/index.vue"),
     },
     {
-      path: ":team_id",
+      path: "/teams/:team_id",
       name: "team",
       component: () => import("@/views/Teams/Team/index.vue"),
     },
     {
-      path: ":team_id/members/:member_id",
+      path: "/teams/:team_id/members/:member_id",
       name: "team-member",
       component: () => import("@/views/Teams/Team/Members/index.vue"),
     },
